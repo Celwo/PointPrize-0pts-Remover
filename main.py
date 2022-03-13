a119a564af94652c9e1535381050ca3c36d5a305
Celwo <83187182+Celwo@users.noreply.github.com>
2022-03-13 21:41:14 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ bad_words = ['Balance = 0', 'Solde : 0']
 
 if not path.exists("Removed"):
         makedirs("Removed")
-with open('hits.txt') as oldfile, open('Removed/pointprize.txt', 'w') as newfile:
+with open('To_remove/hits.txt') as oldfile, open('Removed/pointprize.txt', 'w') as newfile:
     for line in oldfile:
         if not any(bad_word in line for bad_word in bad_words):
             newfile.write(line)
